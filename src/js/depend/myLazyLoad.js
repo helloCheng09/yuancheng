d0feebedccc11ff638cc6288dd3a93b6e2cfe6ea
cheng <1814638551@qq.com>
2019-01-01 12:10:22 +0800

    update 20190101

---
+++ src/js/depend/myLazyLoad.js
@@ -0,0 +1,140 @@
+/**
+ * 实例化 懒加载
+ * loadList 是外层id
+ * obj包含      itemClass: ".load_item", // 加载item
+                data: {
+                     cata_id: '1',
+                     page: 1
+                },// 传输数据
+                url: url, //请求地址
+                success(data):{} //请求成功后的回调
+ */
+class LazyLoad {
+    constructor(loadList, obj) {
+        this.loadList = loadList;
+        this.obj = obj
+        // 开关
+        this.switch = 0
+        // 页码
+        // this.pageNum = 2
+        // 加载控制
+        this.finish = 0
+        // 触发补齐一页
+        this.onePage = 0
+        this.autoPage()
+        // 检测滚动加载
+        // console.log(this.switch)
+    }
+
+    // 检测是否满屏
+    autoPage() {
+        // 500ms监听一次
+        console.log('清空', $(this.loadList))
+        $(this.loadList).empty()
+        var setDefault = setInterval(() => {
+            $('.loadover').remove()
+            if (this.switch == 1)
+                clearInterval(setDefault)
+            else if ($(this.loadList).height() < $(window).height())
+                // 执行
+                this.loadMore($(window)),
+                clearInterval(setDefault)
+            else
+                clearInterval(setDefault)
+        }, 10);
+    }
+
+    // ajax加载
+    loadMore(obj) {
+        let scrollTop = $(obj).scrollTop()
+        let scrollHeight = $(document).height()
+        let windowHeight = $(obj).height()
+        // 完整一个页面
+        if (this.switch == 0 && this.finish == 0 && this.onePage == 0) {
+            if (scrollTop + windowHeight - scrollHeight <= 50) {
+                this.finish = 1
+                this.obj.data.page++
+                // ajax请求接口
+                $.ajax({
+                    url: this.obj.url,
+                    type: "GET",
+                    data: this.obj.data,
+                    dataType: "JSON",
+                    beforeSend: this.loading(),
+                    success: this.obj.success,
+                    // https://www.easy-mock.com/mock/5b9c69299c53ef2876d29227/list/lasyLoad?page=2
+                })
+            }
+        }
+        // 滚动加载
+        if (this.switch == 0 && this.finish == 0) {
+            if (scrollTop > scrollHeight - windowHeight - 50) {
+                this.finish = 1
+                this.obj.data.page++
+                // console.log(2222,this.obj.data)
+                // ajax请求接口
+                $.ajax({
+                    url: this.obj.url,
+                    type: "GET",
+                    data: this.obj.data,
+                    dataType: "JSON",
+                    beforeSend: this.loading(),
+                    success: this.obj.success,
+                    // https://www.easy-mock.com/mock/5b9c69299c53ef2876d29227/list/lasyLoad?page=2
+                })
+            }
+        }
+    }
+
+    render(curPage, totlePage, itemHtml, callback) {
+        // console.log('aaa', curPage, totlePage)
+        clearTimeout(window.timer2)
+        window.timer2 = setTimeout(() => {
+            $(".load-more-bx").remove();
+            $("#myList").append(itemHtml)
+            callback()
+            if (curPage >= totlePage) {
+                this.switch = 1
+                this.overLoading()
+            }
+            this.finish = 0
+            this.onePage = 1
+        }, 200);
+    }
+
+    // 加载动画
+    loading() {
+        var loadHmtl = `  
+        <div class="load-more-bx">
+            <div class="load-more-one">
+                <span class="loading"></span>
+                <span>
+                    加载中..
+                </span>
+            </div>
+        </div>
+        `
+        $(this.loadList).append(loadHmtl);
+    }
+
+    // 加载动画
+    overLoading() {
+        clearTimeout(window.timer1)
+        if (this.switch == 1) {
+            var overtext = "Duang~到底了    ";
+            var overHtml = `
+                <div class="load-more-bx">
+                <div class="loadover"><span class='over-text'>${overtext}</span></div>
+                </div>
+            `
+            window.timer1 = setTimeout(() => {
+                $(this.loadList).append(overHtml);
+            }, 1000);
+        }
+    }
+
+    // error
+    error() {
+        aler("哎呀，加载失败啦~~")
+    }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,16 @@
      * click.js
      */
     (function ($, root) {
+        // 回到顶部
+        function toTop() {
+            $('.go-top').on('click', function () {
+                $('html,body').animate({
+                    scrollTop: '0px'
+                }, 100);
+            })
+        }
+        root.toTop = toTop
+
         // 搜索页面获取 防抖
         /**
          * getSearchVal
@@ -255,6 +265,7 @@
          */
         // root.flTagChoice('.menu-bar', ".bar-item", ".item-text")
         function flTagChoice(panel, btn, styleEle) {
+
             $(panel).each(function () {
                 let outIndex = $(this).index()
                 let curIndex
@@ -263,6 +274,8 @@
                 var that = $(this)
                 that.find(btn).unbind()
                 that.find(btn).on("click", function (e) {
+                    // console.log('清空', $(this.loadList))
+                    // $('#myList').empty()
                     curIndex = $(this).index()
                     console.log('最近点击按钮；' + curIndex)
                     if (curIndex === lastIndex) {
@@ -285,7 +298,7 @@
                                 console.log(i)
                             }
                             var fenleiDataObj = {
-                                cate_id: dataid
+                                cate_id: dataid,
                             }
                             root.modeAjaxGetThd(fenleiUrl, fenleiDataObj)
                         }
@@ -380,17 +393,21 @@
                 var name = item.name
                 var teacher_name = item.teacher_name
                 var title = item.title
-                if (shoufei == 1) {
-                    var sfText = '试听课'
-                    var shoufeiHtml = `
-                        <span class="tag tag-free">${sfText}</span>
-                    `
-                } else {
-                    var shoufeiHtml = ` `
-                    // shoufei = ''
-                    // teacher_name += '(' + school_name + ')'
-                }
-
+                // 收费 试听课
+                // if (shoufei == 1) {
+                //     var sfText = '试听课'
+                //     var shoufeiHtml = `
+                //         <span class="tag tag-free">${sfText}</span>
+                //     `
+                // } else {
+                //     var shoufeiHtml = ` `
+                //     // shoufei = ''
+                //     // teacher_name += '(' + school_name + ')'
+                // }
+                // 学习人数
+                var learning = item.read_num + '人在学习'
+                var learnHtml = `<span class="tag tag-free">${learning}</span> `
+                // var shoufeiHtml = `<span class="tag">${learning}</span> `
                 lesConHtml += `
                 <li class="item-m  mob_1px_b" data-id="${id}">
                         <a href="http://www.mamawozaizhe.com/videos/videos/video_det.html?lesson_id=${id}" class="item-link">
@@ -403,7 +420,6 @@
                                 <div class="les-item-r">
                                     <div class="les-item les-title">
                                         <h3 class="les-text one-ellipsis">${name}</h3>
-                                            ${shoufeiHtml}
                                         </div>
                                     <div class="les-item les-title">
                                         <div class="les-text three-ellipsis">
@@ -412,6 +428,7 @@
                                     </div>
                                     <div class="les-item les-title">
                                         <div class="les-text one-ellipsis les-author">${teacher_name}</div>
+                                        ${learnHtml}
                                     </div>
                                 </div>
                             </div>
@@ -420,22 +437,26 @@
                 `
             })
             $('.les_con_list').empty().append(lesConHtml)
+            root.lcAddPadding()
         }
         root.renderLesCont = renderLesCont
         // 渲染课程中心的课程分类标签
         function renderFenleiTag(data) {
             // 取最后一类
             var len = data.length
+
             var lastTag = data[len - 1][0]
             var finalId = lastTag.id
             var lesDataObj = {
                 cate_id: finalId,
-                page: 1,
+                page: 0,
             }
             $('.insert_con_list').empty()
             var tagListPanel = ''
             // 把最后一个标签id发送后台，渲染页面
-            root.modeAjaxGetSec(kechengUrl, lesDataObj)
+
+            root.lazyLoadLesCenter(kechengUrl, lesDataObj)
+            // root.modeAjaxGetSec(kechengUrl, lesDataObj)
             $.each(data, function (index, item) {
                 tagListPanel = `
                         <div class="menu-bar">
@@ -485,16 +506,19 @@
             } else {
                 // 如果没有下一级
                 // 找到最后一个分类，被选中的分类dataid
-                var finalId = $('.insert_con_list .menu-bar:last-child .select').parent('.bar-item').data('id')
+
+                var finalId = $('.insert_con_list .menu-bar:last-child .select').parent('.bar-item').attr('data-id')
+                console.log(777777777, finalId)
             }
             var lesDataObj = {
                 cate_id: finalId,
-                page: 1,
+                page: 0,
             }
 
             var tagListPanel = ''
             // 把最后一个标签id发送后台，渲染页面
-            root.modeAjaxGetSec(kechengUrl, lesDataObj)
+            root.lazyLoadLesCenter(kechengUrl, lesDataObj)
+            // root.modeAjaxGetSec(kechengUrl, lesDataObj)
             $.each(data, function (index, item) {
                 tagListPanel = `
                         <div class="menu-bar">
@@ -649,6 +673,32 @@
      * init.js
      */
     (function ($, root) {
+        function lcAddPadding() {
+            // 判断手机型号
+            var phonetype = root.phoneType()
+            if (phonetype == 'ios') {
+                $('.les-item .tag-free').css('paddingTop', '2px')
+            }
+        }
+        root.lcAddPadding = lcAddPadding
+        // 判断手机类型
+        function phoneType() {
+            var u = navigator.userAgent,
+                app = navigator.appVersion;
+            var isAndroid = u.indexOf('Android') > -1 || u.indexOf('Linux') > -1; //g
+            var isIOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
+            if (isAndroid) {
+                //这个是安卓操作系统
+                return 'android'
+            } else if (isIOS) {
+                //这个是ios操作系统
+                return 'ios'
+            } else {
+                return 'other'
+            }
+        }
+        root.phoneType = phoneType
+
         // 获取地址中的参数Id
         function getQueryString(id) {
             var reg = new RegExp("(^|&)" + id + "=([^&]*)(&|$)");
@@ -851,6 +901,78 @@
             })
         }
         root.modeAjaxGetSec = modeAjaxGetSec
+
+        // 课程中心 懒加载
+        let lazyLoadLesCenter = (url, data) => {
+            let myLoad = new LazyLoad("#myList", {
+                itemClass: ".load_item",
+                data: data,
+                // data: {
+                //     cata_id: '1',
+                //     page:0
+                // },
+                url: url,
+                success(data) {
+                    console.log(data)
+                    // 获取数据内容
+                    let ajaxData = data
+                    let content = ajaxData.data //具体内容
+                    let curPage = ajaxData.current_page // 当前页码
+                    let totlePage = ajaxData.pages // 总共多少页 通过当前页码和总页码判断是否继续加载
+                    // totlePage = 1
+                    let itemHtml = ''
+                    $.each(content, function (index, item) {
+                        var id = item.id
+                        var shoufei = item.froms
+                        var img = item.img
+                        var school_name = item.school_name
+                        var name = item.name
+                        var teacher_name = item.teacher_name
+                        var title = item.title
+                        var learning = item.read_num + '人在学习'
+                        var learnHtml = `<span class="tag tag-free">${learning}</span> `
+                        itemHtml +=
+                            `
+                            <li class="item-m  mob_1px_b" data-id="${id}">
+                                <a href="http://www.mamawozaizhe.com/videos/videos/video_det.html?lesson_id=${id}" class="item-link">
+                                    <div class="item-b">
+                                        <div class="les-item-l">
+                                            <div class="img-b">
+                                            <div class="img-show" style="background-image:url(${img})"></div>
+                                            </div>
+                                        </div>
+                                        <div class="les-item-r">
+                                            <div class="les-item les-title">
+                                                <h3 class="les-text one-ellipsis">${name}</h3>
+                                                </div>
+                                            <div class="les-item les-title">
+                                                <div class="les-text three-ellipsis">
+                                                ${title}                                      
+                                                </div>
+                                            </div>
+                                            <div class="les-item les-title">
+                                                <div class="les-text one-ellipsis les-author">${teacher_name}</div>
+                                                ${learnHtml}
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                                `
+                    });
+                    myLoad.render(curPage, totlePage, itemHtml, function () {
+                        root.lcAddPadding()
+                    })
+                }
+            })
+            // myLoad.removeContent()
+            //页面滚动执行事件
+            $(window).off()
+            $(window).scroll(function () {
+                myLoad.loadMore($(this));
+            });
+        }
+        root.lazyLoadLesCenter = lazyLoadLesCenter
         // getMode3
         let modeAjaxGetThd = (url, data) => {
             $.ajax({
@@ -927,8 +1049,8 @@
             if (cate_id == '') {
                 var firstDataId = $('.hid-list .hid_item:first-child').data('id')
                 var cateName = $('.hid-list .hid_item:first-child').find('.item-text').text()
-                console.log(1111111111)
-                console.log(firstDataId)
+                // console.log(1111111111)
+                // console.log(firstDataId)
                 firstFlId = firstDataId
             } else {
                 var firstDataId = cate_id
@@ -942,6 +1064,7 @@
             }
             console.log(fenleiDataObj)
             root.modeAjaxGet(fenleiUrl, fenleiDataObj)
+
             // 实例化轮播
             var swiper = new Swiper('.swiper-container', {
                 slidesPerView: 2,
@@ -956,8 +1079,8 @@
             // root.newTagToggle(".bar-item", ".item-text")
             root.newTagToggle(".hid-item", ".hid-item-b")
             root.showMn()
-
-
+            // 回到顶部
+            root.toTop()
         } else if (document.getElementById('centerUser')) {
             // 个人中心
 
@@ -1000,7 +1123,7 @@
                 "components": [{
                     "name": 'MemoryPlayComponent',
                     "type": AliPlayerComponent.MemoryPlayComponent,
-                  }],
+                }],
                 "skinLayout": [{
                         "name": "bigPlayButton",
                         "align": "blabs",
@@ -1075,7 +1198,7 @@
                     $('.prism-fullscreen-btn').show()
                 })
             });
-            
+
             // 确认加入学习
             root.clickPingf()
             root.newTagToggle(".nav-item", ".nav-text", ".det-panel")
@@ -1107,8 +1230,3 @@
     }(window.$, window.wangjiao || (window.wangjiao = {})));
 
 }());
-
-console.log($(".link-back"))
-$("header").on('click', function () {
-    console.log('1aaaaaaaaaaaa')
-})
